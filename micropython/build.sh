1ab3178b84fd0b139e39a8bdedcc9b473d7ec367
ReapeR <hristo.nikodimov.nenkov@gmail.com>
2025-11-28 18:04:25 +0100

    Fixed - passing path to DOCKERHUB_USERNAME

--- micropython/build.sh
+++ micropython/build.sh
@@ -8,13 +8,14 @@ shift
 TAG=""
 MICROPYTHON=""
 ESP_IDF=""
+DIR_NAME=$(basename "$PWD")  # Use basename for Docker image name
 
 # Parse flags, ignore unknown ones
 while [[ $# -gt 0 ]]; do
   case "$1" in
     --tag) TAG="$2"; shift 2 ;;
     --micropython) MICROPYTHON="$2"; shift 2 ;;
-    --esp_idf) ESP_IDF="$2"; shift 2 ;;  # only for esp-idf builds
+    --esp_idf) ESP_IDF="$2"; shift 2 ;;  # optional, only for esp-idf images
     *) shift 2 ;;  # ignore unknown flags
   esac
 done
@@ -30,9 +31,10 @@ echo "  TAG=$TAG"
 echo "  MICROPYTHON=$MICROPYTHON"
 [[ -n "$ESP_IDF" ]] && echo "  ESP_IDF=$ESP_IDF"
 
-# Build command
+# Build arguments
 BUILD_ARGS="--build-arg MICROPYTHON_VERSION=$MICROPYTHON"
 [[ -n "$ESP_IDF" ]] && BUILD_ARGS="$BUILD_ARGS --build-arg ESP_IDF_VERSION=$ESP_IDF"
 
-docker build $BUILD_ARGS -t "$DOCKERHUB_USER/$PWD:$TAG" .
-docker push "$DOCKERHUB_USER/$PWD:$TAG"
+# Build and push
+docker build $BUILD_ARGS -t "$DOCKERHUB_USER/$DIR_NAME:$TAG" .
+docker push "$DOCKERHUB_USER/$DIR_NAME:$TAG"
